13cde8da24f6484459094a39675034b2cfb6a6ed
Andrew Sitzes <andrewsitzes@Andrews-MacBook-Pro.local>
2020-02-27 18:57:21 -0800

    java4

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ class Airplane {
   land() {
     this.isFlying = false;
   }
-}
+} 
 
 /*
 // 👇 COMPLETE YOUR WORK BELOW 👇
@@ -41,9 +41,28 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age){
+    this.name = name,
+    this.age = age,
+    this.stomach = [];
+  }
+  eat(someFood){
+    if (this.stomach.length < 10){
+      return this.stomach.push(someFood);
+    }
+  }
+  poop(){
+      return this.stomach = [];
+    }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
+let newPerson = new Person('andrew', 324);
+console.log(newPerson.toString());
 
+
+//
 /*
   TASK 2
     - Write a Car class whose constructor initializes `model` and `milesPerGallon` from arguments.
@@ -59,7 +78,19 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon){
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  fill(gallons){
+    return this.tank = gallons + this.tank;
+  }
+  drive(distance){
+ this.odometer = distance + this.odometer;
+ this.tank = this.tank - (distance/this.milesPerGallon);
+  }
 }
 
 /*
@@ -74,8 +105,16 @@ class Car {
         + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
         + {name} and {location} of course come from the instance's own properties.
 */
-class Lambdasian {
 
+class Lambdasian {
+  constructor(attr){
+  this.name = attr.name,
+  this.age = attr.age,
+  this.location = attr.location
+}
+  speak(){
+    return `Hello, my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
 /*
@@ -92,10 +131,20 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor(attr){
+  super(attr),
+  this.specialty = attr.specialty,
+  this.favLanguage = attr.favLanguage,
+  this.catchPhrase = attr.catchPhrase
 }
-
+demo(subject){
+  return `Today we are learning about ${subject}`;
+}
+grade(student, subject){
+  return `${student.name} recieves a perfect score on ${subject}`; 
+}
+}
 /*
   TASK 5
     - Write a Student class extending Lambdasian.
@@ -111,7 +160,19 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+  constructor(arg){
+    super(arg),
+    this.previousBackground = arg.previousBackground,
+    this.className = arg.className,
+    this.favSubjects = arg.favSubjects
+  }
+  listSubjects(){
+    return `Loving ${this.favSubjects}!`;
+  }
+  PRAssignment(student, subject){
+    return `${student.name} has submitted a PR for ${subject}`;
+  }
 
 }
 
